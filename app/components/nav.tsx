1d9e98633fa341b39fb0f7f6329b61ad2a5dce91
Joe Palala <joe@palala.me>
2024-12-31 10:26:03 +0800

    Update nav.tsx

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -3,9 +3,8 @@ import { ThemeSwitch } from "./theme-switch";
 import { metaData } from "../config";
 
 const navItems = {
-  "/blog": { name: "Blog" },
   "/projects": { name: "Projects" },
-  "/photos": { name: "Photos" },
+  "/blog": { name: "Blog" },
 };
 
 export function Navbar() {
